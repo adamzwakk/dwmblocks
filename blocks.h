2df386858a16673faa998e087f2d49a682fdafda
Adam Zwakenberg <shamist@gmail.com>
2020-10-31 16:20:32 -0400

    removed blocks I dont use, added volume

--- blocks.h
+++ blocks.h
@@ -1,16 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-
-	{"💽", "nasspace",			3600, 0},
-
-	//{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
-	
 	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "~/.local/bin/statusbar/volume",						0,		10},
+	{"🔈", "amixer sget Master | awk -F\"[][]\" '/dB/ { print $2 }'",	0,		10},
 
-	//{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
+	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
 	
 	//{"", "~/bin/statusbar/battery",						5,		0},
 
